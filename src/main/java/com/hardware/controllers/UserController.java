8afb2bbdea21a95b97ed0e509401f094881c08d9
Saliv Maharjan <vermion00@gmail.com>
2026-05-10 16:49:24 +0545

    Completed CRUD APIS

--- src/main/java/com/hardware/controllers/UserController.java
+++ src/main/java/com/hardware/controllers/UserController.java
@@ -2,17 +2,19 @@ package com.hardware.controllers;
 
 import com.hardware.dtos.UserDTO;
 import com.hardware.services.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
@@ -23,6 +25,26 @@ public class UserController {
     @GetMapping("{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id){
         UserDTO user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.FOUND);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        UserDTO user = userService.updateUser(id, userDTO);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // DELETE USER
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // GET ALL USER
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        List<UserDTO> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
--- src/main/java/com/hardware/services/impl/UserServiceImpl.java
+++ src/main/java/com/hardware/services/impl/UserServiceImpl.java
@@ -6,28 +6,63 @@ import com.hardware.exceptions.ResourceNotFoundException;
 import com.hardware.mapper.UserMapper;
 import com.hardware.repositories.UserRepository;
 import com.hardware.services.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     // Create Users
     @Override
     public UserDTO createUser(UserDTO userDTO) {
         User user = UserMapper.toEntity(userDTO);
-        User savedUser =  userRepository.save(user);
+        User savedUser = userRepository.save(user);
         return UserMapper.toDTO(savedUser);
     }
 
+    // Get User By ID
     @Override
     public UserDTO getUserById(Long id) {
         User user = userRepository.findById(id).
-                orElseThrow(()-> new ResourceNotFoundException("User not found"));
+                orElseThrow(() -> new ResourceNotFoundException("USER ID NOT FOUND"));
         return UserMapper.toDTO(user);
+    }
+
+    // Update User
+    @Override
+    public UserDTO updateUser(Long id, UserDTO userDTO) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("USER ID NOT FOUND"));
+
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setContact(userDTO.getContact());
+        user.setAddress(userDTO.getAddress());
+
+        User updatedUser = userRepository.save(user);
+        return UserMapper.toDTO(updatedUser);
+    }
+
+    // Delete User
+    @Override
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("USER ID NOT FOUND"));
+        userRepository.delete(user);
+    }
+
+    // Get ALl Users
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User>  users = userRepository.findAll();
+        return users.stream().map(UserMapper::toDTO).collect(Collectors.toList());
     }
 }
 
